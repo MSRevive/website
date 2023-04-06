81e7d44e1e8e627cbd6d8ffa9396931b649d373b
Saint Wish <SaintWish@users.noreply.github.com>
2023-04-06 08:32:22 -0600

    have navigation and content box work side by side
    it's also mobile responsive too, with the magic of flex display!

--- src/app.jsx
+++ src/app.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import Sidebar from './components/sidebar/sidebar';
 import NavItem from './components/sidebar/navitem';
-//import Content from './components/content/content';
+import Content from './components/content/content';
 
 function App() {
 	return (
-		<div className="App">
+		<div className="app">
 			<Sidebar>
 				<NavItem icon="fa-home" name="About" />
 				<NavItem icon="fa-download" name="Downloads" />
 				<NavItem icon="fa-info" name="Info" />
 				<NavItem icon="fa-user" name="Dev Team" />
 			</Sidebar>
+
+			<div className="content-wrapper">
+				<Content></Content>
+			</div>
 		</div>
 	);
 }
---
+++ src/components/content/content.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './content.css';
+
+function Content()
+{
+	return (
+		<div className="content">
+			<div className="title">Magical unicorns and pixie dust!</div>
+			<p>
+				Some random text here.
+			</p>
+		</div>
+	);
+}
+
+export default Content;
