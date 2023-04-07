df722c8b37095bf2531c5fcb319bd1d3db07a9f3
Saint Wish <SaintWish@users.noreply.github.com>
2023-04-07 16:16:54 -0600

    logic for page routing and basic pages

--- src/app.jsx
+++ src/app.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { FaDiscord, FaHome, FaDownload, FaInfoCircle, FaUsers } from "react-icons/fa";
 import Sidebar from './components/sidebar/sidebar';
 import NavItem from './components/sidebar/navitem';
 import SocItem from './components/sidebar/socitem';
-import Content from './components/content/content';
-import { FaDiscord, FaHome, FaDownload, FaInfoCircle, FaUsers } from "react-icons/fa";
+import Home from './pages/home';
+import NotFound from './pages/notfound';
+import NotDone from './pages/notdone';
 
 function App() {
 	return (
 		<div className="app">
 			<Sidebar>
-				<NavItem icon={<FaHome />} name="Home" />
-				<NavItem icon={<FaDownload />} name="Downloads" />
-				<NavItem icon={<FaInfoCircle />} name="Info" />
-				<NavItem icon={<FaUsers />} name="Dev Team" />
+				<NavItem link="/" icon={<FaHome />} name="Home" />
+				<NavItem link="/downloads" icon={<FaDownload />} name="Downloads" />
+				<NavItem link="/info" icon={<FaInfoCircle />} name="Info" />
+				<NavItem link="/team" icon={<FaUsers />} name="Dev Team" />
 				<SocItem>
 					<a href="#"><FaDiscord /></a>
 				</SocItem>
@@ -22,17 +25,13 @@ function App() {
 				<div className="content-container">
 					<img src="./assets/hero.png" className="logo"></img>
 
-					<Content title="Unicorns and pixie dust!">
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eget risus in metus venenatis mattis. Quisque odio massa, efficitur quis vehicula vitae, congue nec orci. Nullam id ligula sit amet mauris pharetra hendrerit at in magna. Aenean id sollicitudin arcu, ac lacinia elit. Nunc sed ex dignissim, egestas diam vel, bibendum elit. Duis volutpat efficitur enim a congue. Praesent sodales ligula non semper ultricies. Vestibulum tristique vitae tellus eu volutpat. Suspendisse potenti. Phasellus vel nunc est. Nullam nec laoreet metus. In viverra tristique turpis, non rhoncus nibh euismod at. Integer vestibulum lacus ut lacus sodales faucibus. Donec augue dolor, pulvinar sed aliquam nec, maximus vitae magna. In eleifend orci a enim venenatis, non bibendum nibh gravida.
-
-Vestibulum eget diam bibendum, sollicitudin tortor nec, scelerisque dui. Aenean elementum orci vel tempus ultrices. In non euismod nisi. Morbi pulvinar, justo in viverra commodo, erat diam dictum tortor, sed eleifend nunc magna in eros. Donec eget mi nibh. Aenean vel libero id sem dapibus dapibus in vitae turpis. Donec ultricies nisi et felis rutrum, at fermentum turpis tempus. Aliquam eu nulla vulputate, ornare velit quis, pharetra turpis. Sed pulvinar ligula ut lacus varius consequat. Ut id tempor quam. Donec hendrerit cursus elit, at commodo ante congue ut. Ut nulla nulla, auctor consequat sollicitudin sed, fringilla auctor odio. Quisque id lacus vel quam fermentum sagittis. Etiam mollis odio eget risus egestas mattis nec eu lectus. Donec rutrum ultrices efficitur.
-
-Maecenas augue massa, venenatis luctus condimentum et, interdum ut diam. Proin mattis diam eros, condimentum faucibus purus tempor ac. Nunc id nisi tempor erat tincidunt finibus et eget turpis. Duis sed ligula libero. Phasellus magna augue, iaculis vitae metus ut, laoreet semper sapien. In suscipit interdum faucibus. Pellentesque sit amet fermentum ipsum, in tincidunt nibh. Morbi aliquet tellus finibus diam convallis accumsan. Nunc semper nec neque a aliquet. Ut nisi quam, finibus sed posuere ut, pretium eu nulla. Phasellus vitae rhoncus turpis, nec viverra neque. Vivamus sed tempus felis. Donec pellentesque at odio nec sodales. Nullam enim velit, elementum sed feugiat vel, dapibus in massa. Mauris vitae luctus mauris. Aliquam tristique consequat elit non finibus.
-
-Aenean maximus est at tempor mollis. Nam id ipsum tortor. Donec congue ante lorem, eget tincidunt nulla lobortis ac. Quisque a lorem id purus laoreet hendrerit. Phasellus auctor vestibulum massa, a congue nisl aliquet nec. Etiam porta velit id est vestibulum, eget tempor dolor varius. Ut non enim a magna pellentesque cursus in vitae neque. Nullam ut ultricies nisi, eu sodales turpis. Proin non nulla posuere, commodo sem sed, imperdiet est.
-
-Fusce dapibus, ante et aliquet efficitur, est justo sodales erat, sed finibus libero urna sit amet sapien. Nullam nulla dui, bibendum ac lacus vel, maximus bibendum mauris. Curabitur ut ornare metus. Morbi eget tristique libero. Donec volutpat euismod elit, vel rutrum lectus interdum vitae. Sed ac ipsum nec ligula vehicula rhoncus a quis arcu. Duis pretium libero nec nunc dignissim, ac vehicula turpis convallis. Maecenas at maximus velit. Sed posuere malesuada cursus. Sed pharetra dui in sapien ultrices tempor. Vestibulum ut urna velit. Aliquam at massa metus. Cras ultricies orci vel erat congue, sed pretium odio elementum. Curabitur at metus rhoncus, cursus orci id, laoreet magna. Aliquam dictum metus in maximus bibendum. 
-					</Content>
+					<Routes>
+						<Route path="/" element={<Home />} />
+						<Route path="/downloads" element={<NotDone />} />
+						<Route path="/info" element={<NotDone />} />
+						<Route path="/team" element={<NotDone />} />
+						<Route path="*" element={<NotFound />} />
+					</Routes>
 
 					<div className="footer">&#169; Team MSRebirth. Goldsource & Half-Life are trademarks of Valve Corporation.</div>
 				</div>
--- src/components/sidebar/navitem.jsx
+++ src/components/sidebar/navitem.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 import './sidebar.css';
 
 function NavItem(props) {
+	let link = props.link;
 	let icon = props.icon;
 	let name = props.name;
 
 	return (
 		<div className="sidebar-items">
-			<div className="item">
+			<Link to={link} className="item">
 				<i>{icon}</i>
 				<span className="text">{name}</span>
-			</div>
+			</Link>
 		</div>
 	);
 }
