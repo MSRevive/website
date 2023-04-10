7334b69648ccfaec0386b97db7a3d3656035d9c5
Saint Wish <SaintWish@users.noreply.github.com>
2023-04-10 06:55:23 -0600

    grammar and finish under construction page

--- src/pages/notdone.jsx
+++ src/pages/notdone.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import Box from '../components/content/box'
 
 function NotDone()
 {
 	return (
 		<>
+			<Box>
+				<img src="./assets/notdone.png" alt="not done" width="250" height="250" className="center" />
+				<h1 className="center">Caution! This Page is Not Done</h1>
+				<p className="center">Do not worry, we have hired some brave if not foolish adventurers to transverse through The Wall for the missing pieces.</p>
+			</Box>
 		</>
 	);
 }
