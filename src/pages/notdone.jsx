4fe42912437cf5bd1c910cba72cc8df6acd87477
Saint Wish <SaintWish@users.noreply.github.com>
2023-04-15 11:53:26 -0600

    rearrange

--- src/pages/notdone.jsx
+++ src/pages/notdone.jsx
@@ -6,7 +6,7 @@ function NotDone()
 	return (
 		<>
 			<Box>
-				<img src="./assets/notdone.png" alt="not done" width="250" height="250" className="center" />
+				<img src="./assets/gfx/notdone.png" alt="not done" width="250" height="250" className="center" />
 				<h1 className="center">Caution! This Page is Not Done</h1>
 				<p className="center">Do not worry, we have hired some brave if not foolish adventurers to transverse through The Wall for the missing pieces.</p>
 			</Box>
--- src/pages/notfound.jsx
+++ src/pages/notfound.jsx
@@ -6,7 +6,7 @@ function NotFound()
 	return (
 		<>
 			<Box>
-				<img src="./assets/404.png" alt="404" width="250" height="250" className="center" />
+				<img src="./assets/gfx/404.png" alt="404" width="250" height="250" className="center" />
 				<h1 className="center">This Page is Broken</h1>
 				<p className="center">While exploring some adventurers ran into an old man with a certain blue box in the town of helena. Turns out the old man was just an old crazy man from the local tavern and didn't know how to properly use the blue box and thus broke time. What does this have to do with a certain page missing? Simple, it's time so who the hell knows, y'all shouldn't have been missing with it in the first place!</p>
 			</Box>
